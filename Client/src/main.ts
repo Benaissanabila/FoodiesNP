9392b38756aa1e4dffdb3c7a120ff88ce511cae1
Benaissanabila <benaissa.nabila14@gmail.com>
2024-09-26 15:33:19 -0400

    correction main

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -7,8 +7,6 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-const pinia = createPinia();
-app.use(pinia);
 app.use(createPinia())
 app.use(router)
 app.use(i18n); 
